119ef095e3bf8a539849b7e5d7d2f7b25c3c4dd6
kamalraj18 <kamalselvam18@gmail.com>
2024-01-18 08:36:17 +0530

    final commit

---
+++ main.py
@@ -0,0 +1,43 @@
+import requests
+import selectorlib
+import os
+from send_email import send_email
+
+URL = "https://programmer100.pythonanywhere.com/tours/"
+HEADERS = {
+    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
+
+
+def scrape(url):
+    response = requests.get(url, headers=HEADERS)
+    source = response.text
+    return source
+
+
+def extract(source):
+    extractor = selectorlib.Extractor.from_yaml_file("extract.yaml")
+    value = extractor.extract(source)["tours"]
+    return value
+
+
+def store(extracted):
+    with open('data.txt', 'a') as file:
+        file.write(extracted + '\n')
+
+
+def read(extracted):
+    with open('data.txt', 'r') as file:
+        return file.read()
+
+
+if __name__ == "__main__":
+    scraped = scrape(URL)
+    extracted = extract(scraped)
+    print(extracted)
+    read = read(extracted)
+    body = ''
+    if extracted != "No upcoming tours":
+        if extracted not in read:
+            store(extracted)
+            body = "Subject: new event" + body + "\n" + "new event was there"
+    send_email(message=body)
